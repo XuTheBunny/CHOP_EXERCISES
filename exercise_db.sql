676b60e4753e00d35e84dca185be1e8a4483fdcc
Xu Zhu <32206137+XuTheBunny@users.noreply.github.com>
2018-10-25 14:16:28 -0400

    Update exercise_db.sql

--- exercise_db.sql
+++ exercise_db.sql
@@ -5,13 +5,11 @@ select (select count (id)  from patient where gender = ‘F’) as ‘Female Cou
 -- Data Exercise 2
 select count (distinct patient_id) as ‘DERMATITIS patient count’ from encounter where id in (select encounter_id from encounter_diagnosis where diagnosis_id = (select id from diagnosis where name = ‘DERMATITIS’));
 
-
 -- Data Exercise 3
-select * from patient where id in (select distinct patient_id from encounter where id in (select encounter_id from lab_result where cd4 < 500)) order by mrn; 
+select * from patient where id in (select distinct patient_id from encounter where id in (select encounter_id from lab_result where cd4 < 300)) order by mrn; 
 
 -- Data Exercise 4
 select count (id) as ‘Female patient  count (age above 30)’ from patient where (gender = ‘F’ and birthdate < (select date(‘now’, ‘-30 years’)));
-
 
 -- Bonus Data Exercise
 /*
